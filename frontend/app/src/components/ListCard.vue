4f710b3b9ffe83de14c9877fb7678868bfdd1853
mzw86020424 <mzw86020424@gmail.com>
2022-06-05 16:04:40 +0900

    リストカードの見た目を仮置き2

--- frontend/app/src/components/ListCard.vue
+++ frontend/app/src/components/ListCard.vue
@@ -1,20 +1,23 @@
 <template>
-  <div>
-      <v-card elevation="2" outlined shaped>
-          <v-card-title>TEST</v-card-title>
-          <v-card-subtitle>this is subtitle about test</v-card-subtitle>
-          <v-card-text>this is main text about test. lorem ipsum</v-card-text>
-      </v-card>
-  </div>
+<div>
+    <v-card elevation="5" outlined shaped>
+        <v-img
+        src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+        ></v-img>
+        <v-card-title>TEST</v-card-title>
+        <v-card-subtitle>this is subtitle about test</v-card-subtitle>
+        <v-card-text>this is main text about test. lorem ipsum</v-card-text>
+    </v-card>
+</div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-  name: 'ListCard',
-  props: {
+    name: 'ListCard',
+    props: {
     title: String,
-  },
+    },
 });
 </script>
--- frontend/app/src/components/TitleLogo.vue
+++ frontend/app/src/components/TitleLogo.vue
@@ -3,10 +3,10 @@
     <v-row class="text-center">
       <v-col cols="12">
         <v-img
-          :src="require('../assets/logo.svg')"
+          :src="require('../assets/logo.png')"
           class="my-3"
           contain
-          height="200"
+          height="50"
         />
       </v-col>
     </v-row>
