612dfb736a3204a9ae6a9dc38aea05d907b9c36d
mzw86020424 <mzw86020424@gmail.com>
2022-06-18 15:16:10 +0900

    空白カード作成ボタンを実装

--- frontend/app/src/views/HomeView.vue
+++ frontend/app/src/views/HomeView.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>
+  <div class="text-center">
+    <v-btn
+      class="mx-2"
+      fab
+      dark
+      color="indigo"
+      @click="createCard('')"
+    >
+      <v-icon dark>
+        mdi-plus
+      </v-icon>
+    </v-btn>
     <ul>
         <list-card v-for="item in items.slice().reverse()" :key="item.id" :item="item" @parentMethod="getListCards" />
     </ul>
@@ -9,6 +20,7 @@
 <script lang="ts">
 import Vue from 'vue'
 import ListCard from '../components/ListCard.vue';
+import axios from 'axios';
 
 export default Vue.extend({
   name: 'HomeView',
@@ -28,11 +40,25 @@ export default Vue.extend({
       await this.$store.dispatch('fetch_items');
       this.items = this.$store.getters.items;
     },
+
+    async createCard(title: string) {
+      // await this.$store.dispatch('post_item', "");
+      await axios
+      .post("http://localhost:8000/tasks", {
+      title: title,
+      })
+      .then(
+          (res) => res.data,
+          () => ''
+      );
+      this.getListCards();
+    }
   },
 
   async created() {
     await this.getListCards();
     this.items = this.$store.getters.items;
+    await this.$store.dispatch('post_item', "");
   },
 
 });
